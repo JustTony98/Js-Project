cddf8aef8d0f9f288253aba4e504c62815f28cf0
JustTony98 <tonydm9886@gmail.com>
2023-02-01 14:56:51 +0300

    chng js, lesson02

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-let title = "";
-let screens = "";
-let screenPrice = "";
-let rollback = "";
-let fullPrice = "";
-let adaptive = "";
+let title = "Js-Project";
+let screens = "Простые, Сложные, Интерактивные";
+let screenPrice = 25000;
+let rollback = 77;
+let fullPrice = 100000;
+let adaptive = "true";
 
 alert(
   "Что вершит судьбу человечества в этом мире? Некое незримое существо или закон, подобно Длани Господней парящей над миром?"
@@ -11,3 +11,12 @@ alert(
 console.log(
   "По крайне мере истинно то, что человек не властен даже над своей волей."
 );
+
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+console.log(screens.length);
+console.log(`Стоимость верстки экранов ${screenPrice} рублей, \nСтоимость разработки сайтов 
+${fullPrice} рублей`);
+console.log(screens.toLowerCase().split());
+console.log(fullPrice * (rollback / 100));
