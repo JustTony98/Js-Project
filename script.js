07fd21af505b0b8eaf603fe010af38ead0d6b803
JustTony98 <tonydm9886@gmail.com>
2023-02-02 21:28:39 +0300

    fixes lesson03

--- script.js
+++ script.js
@@ -1,54 +1,29 @@
 "use strict";
 
-let title = "Js-Project";
-let screens = "Простые, Сложные, Интерактивные";
-let screenPrice = 25000;
 let rollback = 20;
-let fullPrice = 100000;
-let adaptive = "true";
-
-/* alert(
-  "Что вершит судьбу человечества в этом мире? Некое незримое существо или закон, подобно Длани Господней парящей над миром?"
-);
-console.log(
-  "По крайне мере истинно то, что человек не властен даже над своей волей."
-); */
-
-console.log(typeof title);
-console.log(typeof fullPrice);
-console.log(typeof adaptive);
-console.log(screens.length);
-console.log(`Стоимость верстки экранов ${screenPrice} рублей, \nСтоимость разработки сайтов 
-${fullPrice} рублей`);
-console.log(screens.toLowerCase().split(", "));
-console.log(fullPrice * (rollback / 100));
-
-title = prompt("Как называется ваш проект?");
-console.log(title);
-screens = prompt("Какие типы экранов нужно разработать?");
-console.log(screens);
-screenPrice = prompt("Сколько будет стоить данная работа?");
-console.log(screenPrice);
-adaptive = prompt("Нужен ли адаптив на сайте?");
-console.log(Boolean(adaptive));
+let title = prompt("Как называется ваш проект?");
+let screens = prompt("Какие типы экранов нужно разработать?");
+let screenPrice = +prompt("Сколько будет стоить данная работа?");
+let adaptive = confirm("Нужен ли адаптив на сайте?");
 let service1 = prompt("Какой дополонительный тип услуг вам нужен?");
-console.log(service1);
-let servicePrice1 = prompt("Сколько это будет стоить?");
-console.log(servicePrice1);
+let servicePrice1 = +prompt("Сколько это будет стоить?");
 let service2 = prompt("Какой дополонительный тип услуг вам нужен?");
-console.log(service2);
-let servicePrice2 = prompt("Сколько это будет стоить?");
-console.log(servicePrice2);
-fullPrice = +screenPrice + +servicePrice1 + +servicePrice2;
-console.log(fullPrice);
-let servicePercentPrice = fullPrice - (fullPrice / 100) * 10;
-console.log(Math.ceil(servicePercentPrice));
+let servicePrice2 = +prompt("Сколько это будет стоить?");
+let fullPrice = +screenPrice + +servicePrice1 + +servicePrice2;
+let servicePercentPrice = fullPrice - fullPrice * (rollback / 100);
+
 if (fullPrice >= 30000) {
   console.log("Дарим скидку в 10%");
-} else if (15000 <= fullPrice && fullPrice < 30000) {
+} else if (fullPrice >= 15000 && fullPrice < 30000) {
   console.log("Даем скидку в 5%");
-} else if (15000 > fullPrice && fullPrice >= 0) {
+} else if (fullPrice >= 0 && fullPrice < 15000) {
   console.log("Скидка не предусмотрена");
-} else if (Math.sign(fullPrice)) {
+} /* if (Math.sign(fullPrice)) */ else {
   console.log("Что то пошло не так");
 }
+
+console.log(title);
+console.log(screenPrice);
+console.log(typeof adaptive);
+console.log(fullPrice);
+console.log(servicePercentPrice);
